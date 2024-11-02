2e42f3f51204f869cfc4ee67195a4340e4f0781e
Dakota <dakotajacksonlane@gmail.com>
2024-11-02 17:01:55 +0800

    JS 4-7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,34 @@
-const removeFromArray = function() {
-};
+/*const removeFromArray = function(input) {
+    let output = [];
+    const toRemove = Array.from(arguments);
+    input.forEach(element => {
+        let push = true;
+        for (let ii = 1; ii < toRemove.length; ii++) //ignore the first element (0) as it would be the input.
+        {
+            if(element === toRemove[ii]) {
+                push = false;
+            }
+        }
+        if(push) {
+            output.push(element);
+        }
+    });
+    return output;
+};*/
+
+const removeFromArray = function(input, ...args) {
+    const output = [];
+    input.forEach(element => {
+        if (!args.includes(element)) {
+            output.push(element);
+        }
+    });
+    return output;
+}
+
+
+
+removeFromArray([1,2,3,4], 2, 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,28 @@
-const leapYears = function() {
+/*const leapYears = function(year) {
+    if(year%400 == 0)
+        return true;
+    else if (year %100 == 0)
+        return false;
+    else if (year %4 == 0)
+        return true;
+    else
+        return false; 
+};*/
 
-};
 
+const leapYears = function (year) {
+    const isYearDivisibleByFour = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const isYearDivisibleByFourHundred = year % 400 === 0;
+  
+    if (
+      isYearDivisibleByFour &&
+      (!isCentury || isYearDivisibleByFourHundred)
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 // Do not edit below this line
 module.exports = leapYears;
